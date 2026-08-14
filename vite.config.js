97f61bfe1be61dc95c3f43823769727d52af4b8c
Agbaje Joshua <agbajejoshua36@gmail.com>
2026-08-14 07:30:04 +0100

    fix: add vite-plugin-node-polyfills to resolve buffer error on complete-profile route

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
 import { defineConfig } from "vite";
-
 import react from "@vitejs/plugin-react";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import { VitePWA } from "vite-plugin-pwa";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
-
 export default defineConfig({
   plugins: [
     react(),
-
     tailwindcss(),
+    nodePolyfills({
+      include: ['buffer'],
+      globals: {
+        Buffer: true,
+      },
+    }),
 
     VitePWA({
       registerType:
